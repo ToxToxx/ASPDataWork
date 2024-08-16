ab7af850df99b15874258a116cd11b7377f076de
Matvey <sinema3d00@gmail.com>
2024-08-16 13:57:45 +0300

    made User model

---
+++ ASPDataWork/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace ASPDataWork.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public User() { }
+
+        public User(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
