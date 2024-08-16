edb27ef0f70313c141dfd6f8799b43c985af1ba9
Matvey <sinema3d00@gmail.com>
2024-08-16 15:32:30 +0300

    add view model to use it when adding user

---
+++ ASPDataWork/Models/CreateUserViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASPDataWork.Models
+{
+    public class CreateUserViewModel
+    {
+        [Required(ErrorMessage = "Введите имя")]
+        [MaxLength(50, ErrorMessage = "Длина не должна быть больше 50 символов")]
+        public string Name { get; set; }
+
+        [Range(1, 110, ErrorMessage = "Недопустимыый возраст")]
+        public int Age { get; set; }
+    }
+}
