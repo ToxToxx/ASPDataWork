54d781dcf36bc6eca776f4a288e2cd4d8b703f41
Matvey <sinema3d00@gmail.com>
2024-08-16 15:32:11 +0300

    made Service realization

---
+++ ASPDataWork/Models/UserService.cs
@@ -0,0 +1,27 @@
+﻿using ASPDataWork.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPDataWork.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly AppDbContext _context;
+
+        public UserService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddUser(string name, int age)
+        {
+            var user = new User { Name = name, Age = age };
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<User>> GetAllUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+    }
+}
