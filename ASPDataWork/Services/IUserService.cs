167f91beabd1c0b9bac2ad85f70c8d47e98f366f
Matvey <sinema3d00@gmail.com>
2024-08-16 15:31:47 +0300

    create service for users

---
+++ ASPDataWork/Services/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using ASPDataWork.Models;
+
+namespace ASPDataWork.Services
+{
+    public interface IUserService
+    {
+        Task AddUser(string name, int age);
+        Task<IEnumerable<User>> GetAllUsers();
+    }
+}
