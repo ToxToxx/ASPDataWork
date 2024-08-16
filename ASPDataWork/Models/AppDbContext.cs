53940ec6edc24c505e19c0e760287db484c43837
Matvey <sinema3d00@gmail.com>
2024-08-16 15:32:38 +0300

    add db context

---
+++ ASPDataWork/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ASPDataWork.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
