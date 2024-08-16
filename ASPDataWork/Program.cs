e2844b29adf51614991dd110dbe6faba9017f904
Matvey <sinema3d00@gmail.com>
2024-08-16 15:32:02 +0300

    connect services and Db

--- ASPDataWork/Program.cs
+++ ASPDataWork/Program.cs
@@ -1,15 +1,23 @@
+using ASPDataWork.Models;
+using ASPDataWork.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Подключение к базе данных
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Регистрация сервисов
+builder.Services.AddScoped<IUserService, UserService>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
